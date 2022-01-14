17e0a709e62ab6e5912402185da5e5a06eb29ab2
amirhallaji <a.hallaji.b@gmail.com>
2022-01-14 15:18:47 +0330

    feat: hw2 world added.

--- controllers/my_controller/my_controller.py
+++ controllers/my_controller/my_controller.py
@@ -1,11 +1,45 @@
-from controller import Robot
+from controller import Robot, Lidar, Camera
+import csv
 
-robot = Robot()
-
-timestep = int(robot.getBasicTimeStep())
-
-while robot.step(timestep) != -1:
-
-    pass
+MAX_SPEED = 6.28
 
 
+if __name__ == '__main__':
+    
+    robot = Robot()
+    timestep = int(robot.getBasicTimeStep())
+
+    leftMotor = robot.getDevice('left wheel motor')
+    rightMotor = robot.getDevice('right wheel motor')
+    leftMotor.setPosition(float('inf'))
+    rightMotor.setPosition(float('inf'))
+    
+    leftMotor.setVelocity(0)
+    rightMotor.setVelocity(0) 
+    
+    lidar = Lidar('lidar')
+    lidar.enable(timestep)
+    range_image = []
+    
+    camera = Camera('camera')
+    camera.enable(timestep)
+    
+    
+    counter = 0;
+    
+    while robot.step(timestep) != -1 and counter != 100:
+        counter += 1
+        camera.saveImage('imageCamera.png', 100)
+        # range_image = lidar.getRangeImage()
+        # print("{}".format(range_image))
+        # print(len(range_image))
+    
+       
+    # f = open('data.csv', 'w')
+    # writer = csv.writer(f)        
+    # writer.writerow(range_image)
+    # f.close()
+        
+        # leftMotor.setVelocity(MAX_SPEED * 0)
+        # rightMotor.setVelocity(MAX_SPEED * 0)
+        
